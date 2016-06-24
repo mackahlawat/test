85ce129b1f3b1d4240b6f08501929e7ca9a37ce8
Manjeet <manjeet@local.com>
2016-06-23 18:57:28 -0700

    putiing dashes

--- src/com/sandeep/test/exceptions/ExceptionDemo2.java
+++ src/com/sandeep/test/exceptions/ExceptionDemo2.java
@@ -2,6 +2,6 @@ package com.sandeep.test.exceptions;
 
 public class ExceptionDemo2 {
 	public static void main(String[] args) {
-		System.out.println("this is a new demo");
+		System.out.println("this is a new demo   ");
 	}
 }
